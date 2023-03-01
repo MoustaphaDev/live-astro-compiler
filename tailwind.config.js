688e4c996bf8d31dd17495982208793c5b7f4a57
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-01 21:19:28 +0000

    feat: add custom colors to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000ef",
+        secondary: "#323232",
+        "accent-1": "#FF1E56",
+        "accent-2": "#FFAC41",
+        // "accent-2-dim": "hsl(6, 100%, 80%)",
+      },
+      fontFamily: {
+        "fira-code": ["Fira Code", "monospace"],
+      },
+      animation: {
+        "fade-in": "fade-in 300ms ease-out",
+        "fade-out": "fade-out ease-in forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+        "fade-out": {
+          from: {
+            opacity: "1",
+            top: "0",
+          },
+          to: {
+            opacity: "0",
+          },
+        },
+      },
+    },
+  },
 };
