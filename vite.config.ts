dcc405c37feb20b52cabe7d960166382072b3684
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-01 20:55:52 +0000

    chore: setup dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [devtools({ autoname: true }), solidPlugin(), tsConfigPaths()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
