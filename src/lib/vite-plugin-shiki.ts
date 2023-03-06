c8360297a123d4d3cf21ac397dd5f9aab6daafa9
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-06 14:41:13 +0000

    feat: vite plugin to copy shiki files to `/public`
    Also injects code to load the oniguruma wasm

---
+++ src/lib/vite-plugin-shiki.ts
@@ -0,0 +1,71 @@
+// import { setCDN } from "shiki";
+import type { Plugin, ResolvedConfig } from "vite";
+import fs from "node:fs";
+import { fileURLToPath, pathToFileURL } from "node:url";
+import npath from "node:path";
+import { red, blue, green } from "kleur";
+
+/**
+ * Copies the shiki folder to the `/public/shiki` folder when they don't exist already
+ */
+export function vitePluginShiki({
+  pathToInitializedHighlighter: entrypoint,
+}: {
+  pathToInitializedHighlighter: string;
+}): Plugin {
+  const shikiFolderPath = fileURLToPath(
+    new URL("../../node_modules/shiki", import.meta.url)
+  );
+
+  // setCDN(shikiFolderPath.href);
+  let config: ResolvedConfig;
+  return {
+    name: "vite-plugin-shiki",
+    configResolved(_config) {
+      config = _config;
+      if (!fs.existsSync(shikiFolderPath)) {
+        console.error(
+          red(
+            "shiki folder doesn't exist, please install `shiki` package to use this plugin"
+          )
+        );
+        return;
+      }
+
+      const { publicDir } = config;
+      const destinationPath = npath.join(publicDir, "shiki");
+
+      if (fs.existsSync(destinationPath)) {
+        console.info(blue("shiki folder already exists in `public` folder"));
+        return;
+      }
+
+      //  copies the shiki folder to the `/public/shiki` folder when they don't exist already
+      try {
+        fs.cpSync(shikiFolderPath, destinationPath, {
+          recursive: true,
+        });
+        console.info(green("Copied shiki folder to `public` folder"));
+      } catch {
+        console.error(red("Error copying shiki folder to `public` folder"));
+      }
+    },
+    transform(code, id) {
+      const entrypointPath = npath.join(config.root, entrypoint);
+      if (id !== entrypointPath) {
+        return;
+      }
+      console.log({ id, entrypointPath });
+      code = `import { setCDN, setWasm } from "shiki";
+setCDN("/shiki/");
+const wasmResponse = await fetch("/shiki/dist/onig.wasm");
+setWasm(wasmResponse);
+
+${code}`;
+      console.log({ code });
+      return {
+        code,
+      };
+    },
+  };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { vitePluginShiki } from "./src/lib/vite-plugin-shiki";
 
 export default defineConfig({
-  plugins: [devtools({ autoname: true }), solidPlugin(), tsConfigPaths()],
+  plugins: [
+    devtools({ autoname: true }),
+    solidPlugin(),
+    tsConfigPaths(),
+    vitePluginShiki({ pathToInitializedHighlighter: "/src/lib/utils.ts" }),
+  ],
   server: {
     port: 3000,
   },
