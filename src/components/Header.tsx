49bb52fb737f27c57c6d3a92ac3091ae67fbb061
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-12 21:55:18 +0000

    add wrap toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { FaSolidGears } from "solid-icons/fa";
 import { HiOutlineSelector } from "solid-icons/hi";
 import AstroLogo from "~/assets/astro-logo.svg";
+import { TbTextWrap } from "solid-icons/tb";
 
 import { For } from "solid-js";
-import { Select as SelectPrimitive } from "@kobalte/core";
-import { mode, setMode } from "../lib/store";
+import { Select as SelectPrimitive, ToggleButton } from "@kobalte/core";
+import { isWordWrap, mode, setMode, setWordWrap } from "../lib/store";
 
 import { type ComponentProps, splitProps } from "solid-js";
 import { AiOutlineCheck } from "solid-icons/ai";
@@ -51,6 +52,29 @@ function Select(
   );
 }
 
+function WordWrapToggle() {
+  return (
+    <ToggleButton.Root
+      isPressed={isWordWrap()}
+      onPressedChange={setWordWrap}
+      class="box-content h-10 w-[calc(theme(spacing.8)*3)] rounded-full border border-solid border-secondary bg-primary leading-none text-white outline-none ring-offset-0 ring-offset-primary transition-shadow duration-[250ms,color] hover:border-accent-2/50 hover:bg-accent-2/10 focus:ring-2 focus:ring-accent-2"
+      aria-label="Toggle word wrap"
+    >
+      <span
+        class={`ml-1 flex h-8 w-8 items-center justify-center rounded-full transition-all ${
+          isWordWrap()
+            ? "bg-secondary"
+            : "translate-x-[calc(200%-theme(spacing.2))] bg-accent-2"
+        }`}
+      >
+        <TbTextWrap class="text-xl" />
+      </span>
+    </ToggleButton.Root>
+  );
+}
+// .select__trigger[data-invalid] {
+//   @apply border-[hsl(0_72%_51%)] text-[hsl(0_72%_51%)];
+// }
 // function ThemeSwitcher() {
 //   return (
 //     <Select
@@ -93,12 +117,13 @@ export function Header() {
   return (
     <div class="flex h-20 items-center justify-between border-b-2 border-secondary bg-primary px-8">
       <h1 class="flex items-center">
-        <img src={AstroLogo} class="h-16" />
+        <img src={AstroLogo} class="h-14" />
         <FaSolidGears />
         <span class="text-3xl font-extrabold text-white">Live Compiler</span>
       </h1>
       <span class="flex items-center gap-5">
         <ModeSwitcher />
+        <WordWrapToggle />
         {/* <ThemeSwitcher /> */}
       </span>
     </div>
