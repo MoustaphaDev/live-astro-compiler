4eb58dff43462fcd11af391a69f4ed5fd9db299f
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-12-11 15:57:40 +0000

    feat: add vercel analytics

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,11 +1,12 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-
+import { inject } from '@vercel/analytics';
 import App from "./App";
 import { patchGlobals } from "./lib/utils";
 const root = document.getElementById("root");
 
 patchGlobals();
+inject();
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
