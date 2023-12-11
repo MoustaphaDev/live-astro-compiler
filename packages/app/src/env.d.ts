73ee0ce6b95202f9e4461dbcad2d241bdeac1eba
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-12-11 16:36:27 +0000

    chore: remove unused .dts types and npm script

--- packages/app/src/env.d.ts
+++ packages/app/src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly RELATIVE_PATH_TO_COMPILER_DIST_FOLDER: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
