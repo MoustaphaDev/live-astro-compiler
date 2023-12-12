0bd54fe189feb66c545f2b620c25eff3e66c9dd8
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-12-12 00:39:18 +0000

    chore: add vercel speed-insight package

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,12 +1,17 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { inject } from '@vercel/analytics';
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
 import App from "./App";
 import { patchGlobals } from "./lib/utils";
 const root = document.getElementById("root");
 
 patchGlobals();
 inject();
+// @ts-expect-error the type definition is wrong
+// an argument shouldn't be mendatory
+injectSpeedInsights();
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
